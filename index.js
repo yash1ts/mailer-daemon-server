0cec1c982cfafb1ecd19890e7e8ccaab3b581825
yash1ts <yashwantsandey2000@gmail.com>
2020-12-19 19:24:35 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
